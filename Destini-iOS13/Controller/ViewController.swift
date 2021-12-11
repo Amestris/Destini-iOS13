c3b615e6cc838d43e852ae7cc4ec43919059fb0a
Amestris <eemija@gmail.com>
2021-12-11 21:36:16 +0100

    Finished project with related stories

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let story0 = "You see a fork in the road."
+    let choice1 = "Take a left."
+    let choice2 = "Take a right."
+    var storyBrain =  StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
+        updateUI()
+        
     }
 
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        updateUI()
+    }
+    
+    func updateUI(){
+        
+        storyLabel.text = storyBrain.stories[storyBrain.storyNumber].title
+        
+        choice1Button.setTitle(storyBrain.stories[storyBrain.storyNumber].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[storyBrain.storyNumber].choice2, for: .normal)
+        
+    }
 
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+struct Story{
+    var title: String
+    var choice1: String
+    var choice2: String
+    var storyAfter1: Int
+    var storyAfter2: Int
+    
+    init(t: String, c1: String, c2: String, after1: Int, after2: Int){
+        self.title = t
+        self.choice1 = c1
+        self.choice2 = c2
+        self.storyAfter1 = after1
+        self.storyAfter2 = after2
+    }
+    
+}
